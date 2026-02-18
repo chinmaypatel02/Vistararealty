577c4e4c3846ec50e28f8ccdaacefed2a201896e
Chinmay Patel <chinmaypatel14122002@gmail.com>
2026-02-18 17:05:58 +0530

    all working with bottomtabnavigation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
+import 'react-native-gesture-handler'; // MUST BE FIRST LINE
 import { name as appName } from './app.json';
 import { enableScreens } from 'react-native-screens';
 
--- src/navigation/Appnavigator.js
+++ src/navigation/Appnavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Companyscreen from '../screens/Companyscreen/Homescreen';
 import LoginScreen from '../screens/Loginscreen/Loginscreen';
+import BottomTabNavigator from './BottomTabNavigator';
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
@@ -16,6 +17,7 @@ const AppNavigator = () => {
       >
         <Stack.Screen name="Home" component={Companyscreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Dashboard" component={BottomTabNavigator} />
            {/* <Stack.Screen name="Dashboard" component={DrawerNavigator} /> */}
         {/* <Stack.Screen name="Dashboard" component={DashboardScreen} /> */}
       </Stack.Navigator>
